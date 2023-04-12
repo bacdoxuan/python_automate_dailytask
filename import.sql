5e9a5bbef70a9e85c7eb98ed2675620bc3d01abd
DoXuanBac <bacdoxuan@gmail.com>
2023-04-12 12:05:06 +0700

    add import.sql

---
+++ import.sql
@@ -0,0 +1,15 @@
+.open "c:/Bac.DX/SQLite/DataBase/2023_3G Cell HS USER TP.db3"
+.import --csv c:/Users/bac_dx/Desktop/3G_HS_User.csv DailyData
+.quit
+
+.open "c:/Bac.DX/SQLite/DataBase/2023_3G Site traffic Utilization.db3"
+.import --csv c:/Users/bac_dx/Desktop/3G_Site_Utilization.csv DailyData
+.quit
+
+.open "c:/Bac.DX/SQLite/DataBase/2023_4G Cell User Throughput.db3"
+.import --csv c:/Users/bac_dx/Desktop/4G_Cell_TP.csv DailyData
+.quit
+
+.open "c:/Bac.DX/SQLite/DataBase/2023_4G site traffic Utilization.db3"
+.import --csv c:/Users/bac_dx/Desktop/4G_Site_Util.csv DailyData
+.quit
